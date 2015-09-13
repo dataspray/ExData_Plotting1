e12f28baee28ded8d37dc09b3e2baa6be63598e4
dataspray <beansbeforebreakfast@gmail.com>
2015-09-12 23:13:57 -0500

    updates

--- plot1.R
+++ plot1.R
@@ -1,11 +1,10 @@
+library(lubridate, dplyr)
 
-library(lubridate)
-library(dplyr)
 t <- tbl_df(read.csv("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?"))
 t <- mutate(t, DateTime = dmy(Date) + hms(Time))  # Add a datetime column combining the seperate date and time columns
 d <- filter(t, DateTime >= ymd("2007-02-01"), DateTime < ymd("2007-02-03"))
 
 hist(d$Global_active_power,right=FALSE,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)",xaxt="n")
-axis(1, at=seq(0,6,by=2))  # add x labels
+axis(1, at=seq(0,5,by=2))  # add x labels
 dev.copy(png,"plot1.png")
 dev.off()
